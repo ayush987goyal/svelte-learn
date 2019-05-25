59073b24e61bf4c9b1b4b446a3265595a5874f6a
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-25 15:49:59 +0530

    Init basic app with styles

---
+++ learn/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let name = "Ayush";
+  let age = 23;
+
+  const changeAge = () => {
+    age += 1;
+  };
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click={changeAge}>Change Age</button>
