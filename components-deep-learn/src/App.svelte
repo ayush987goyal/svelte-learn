1e59c54cb6b3edfec1b49b5b794f5d31c8720956
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-26 19:07:30 +0530

    Add custom events

--- components-deep-learn/src/App.svelte
+++ components-deep-learn/src/App.svelte
@@ -1,3 +1,8 @@
 <script>
-
+  import Product from "./Product.svelte";
 </script>
+
+<Product
+  productTitle="A Book"
+  on:add-to-cart={() => alert('Added to cart!')}
+  on:delete={() => alert('Deleted!')} />
