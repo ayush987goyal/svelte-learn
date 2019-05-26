f7add22b3bb64e8b69e56113e92257cd07c56fec
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-26 20:04:51 +0530

    Using tick

--- components-deep-learn/src/App.svelte
+++ components-deep-learn/src/App.svelte
@@ -1,8 +1,12 @@
 <script>
+  import { tick } from "svelte";
+
   import Product from "./Product.svelte";
   import Modal from "./Modal.svelte";
 
   let products = [{ id: "p1", title: "A Book", price: 9.99 }];
+
+  let text = "This is some dummy text";
 
   let showModal = false;
   let closable = false;
@@ -13,6 +17,28 @@
 
   function deleteProduct(event) {
     console.log(event);
+  }
+
+  function transform(event) {
+    if (event.which !== 9) {
+      return;
+    }
+
+    event.preventDefault();
+
+    const selectionStart = event.target.selectionStart;
+    const selectionEnd = event.target.selectionEnd;
+    const value = event.target.value;
+
+    text =
+      value.slice(0, selectionStart) +
+      value.slice(selectionStart, selectionEnd).toUpperCase() +
+      value.slice(selectionEnd);
+
+    tick().then(() => {
+      event.target.selectionStart = selectionStart;
+      event.target.selectionEnd = selectionEnd;
+    });
   }
 </script>
 
@@ -37,3 +63,5 @@
     </button>
   </Modal>
 {/if}
+
+<textarea rows="5" bind:value={text} on:keydown={transform} />
