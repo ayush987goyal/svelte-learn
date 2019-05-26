22e93834bf214f79e9bf81ef2942b4d1682338dd
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-26 19:57:16 +0530

    using lifecycle events incl before afterUpdate

--- components-deep-learn/src/Modal.svelte
+++ components-deep-learn/src/Modal.svelte
@@ -1,9 +1,39 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import {
+    createEventDispatcher,
+    onMount,
+    onDestroy,
+    beforeUpdate,
+    afterUpdate
+  } from "svelte";
 
   const dispatch = createEventDispatcher();
 
   let agreed = false;
+  let autoScroll = false;
+
+  onMount(() => {
+    console.log("onMount");
+  });
+
+  onDestroy(() => {
+    console.log("onDestroy");
+  });
+
+  beforeUpdate(() => {
+    console.log("beforeUpdate");
+    autoScroll = agreed;
+  });
+
+  afterUpdate(() => {
+    console.log("afterUpdate");
+    if (autoScroll) {
+      const modal = document.querySelector(".modal");
+      modal.scrollTo(0, modal.scrollHeight);
+    }
+  });
+
+  console.log("Script executed!");
 </script>
 
 <style>
@@ -23,7 +53,7 @@
     top: 10vh;
     left: 10%;
     width: 80%;
-    max-height: 80vh;
+    max-height: 15vh;
     background: white;
     border-radius: 5px;
     z-index: 100;
