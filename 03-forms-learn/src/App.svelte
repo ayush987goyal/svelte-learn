cd22fb8dc044ce5befee4a6b0a4b56913872d050
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-27 09:26:27 +0530

    Select and multi checks

--- 03-forms-learn/src/App.svelte
+++ 03-forms-learn/src/App.svelte
@@ -7,12 +7,16 @@
   let price = 10;
   let agreed = false;
   let favColor = "green";
+  let multiFavColor = ["green"];
+  let singleFavColor = "red";
 
   $: console.log(val);
   $: console.log(selectedOption);
   $: console.log(price);
   $: console.log(agreed);
   $: console.log(favColor);
+  $: console.log(multiFavColor);
+  $: console.log(singleFavColor);
 </script>
 
 <CustomInput bind:val />
@@ -39,3 +43,31 @@
   <input type="radio" name="colors" value="blue" bind:group={favColor} />
   Blue
 </label>
+
+<h1>Favorite multi-color?</h1>
+<label>
+  <input type="checkbox" name="colors" value="red" bind:group={multiFavColor} />
+  Red
+</label>
+<label>
+  <input
+    type="checkbox"
+    name="colors"
+    value="green"
+    bind:group={multiFavColor} />
+  Green
+</label>
+<label>
+  <input
+    type="checkbox"
+    name="colors"
+    value="blue"
+    bind:group={multiFavColor} />
+  Blue
+</label>
+
+<select bind:value={singleFavColor}>
+  <option value="green">Green</option>
+  <option value="red">Red</option>
+  <option value="blue">Blue</option>
+</select>
