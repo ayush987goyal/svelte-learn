166b3f271987dba2504e8c623287604f75cfb4cb
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-28 22:41:30 +0530

    Add some transitions

--- 05-animations-transitions/src/App.svelte
+++ 05-animations-transitions/src/App.svelte
@@ -2,6 +2,7 @@
   // import { writable } from "svelte/store";
   import { tweened } from "svelte/motion";
   import { cubicIn } from "svelte/easing";
+  import { fade, fly, slide, scale } from "svelte/transition";
 
   import Spring from "./Spring.svelte";
 
@@ -11,10 +12,43 @@
     easing: cubicIn
   });
 
+  let boxInput;
+
   setTimeout(() => {
     progress.set(0.5);
   }, 1500);
+
+  let boxes = [];
+
+  function addBox() {
+    boxes = [...boxes, boxInput.value];
+  }
+
+  function discard(value) {
+    boxes = boxes.filter(item => item !== value);
+  }
 </script>
 
+<style>
+  div {
+    width: 10rem;
+    height: 10rem;
+    background: #ccc;
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 5px;
+    padding: 1rem;
+  }
+</style>
+
 <!-- <progress value={$progress} /> -->
-<Spring />
+<!-- <Spring /> -->
+
+<input type="text" bind:this={boxInput} />
+<button on:click={addBox}>Add</button>
+
+{#each boxes as box (box)}
+  <div transition:fly={{ x: 200, y: 3 }} on:click={discard.bind(this, box)}>
+     {box}
+  </div>
+{/each}
