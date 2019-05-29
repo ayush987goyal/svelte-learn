205ce6b48a9a2db0c4fa1fd12802875602f80db1
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-29 07:51:54 +0530

    Add basic http req to firebase

--- 06-http/src/App.svelte
+++ 06-http/src/App.svelte
@@ -1,5 +1,32 @@
 <script>
+  import { API_URL } from "./config.js";
 
+  let hobbyInput;
+  let hobbies = [];
+
+  function addHobby() {
+    hobbies = [...hobbies, hobbyInput.value];
+
+    fetch(`${API_URL}/hobbies.json`, {
+      method: "POST",
+      body: JSON.stringify(hobbies),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Failed!");
+        }
+      })
+      .catch(err => console.log(err));
+  }
 </script>
 
+<label for="hobby">Hobby</label>
+<input type="text" id="hobby" bind:this={hobbyInput} />
+<button on:click={addHobby}>Add Hobby</button>
 
+<ul>
+  {#each hobbies as hobby}
+    <li>{hobby}</li>
+  {/each}
+</ul>
