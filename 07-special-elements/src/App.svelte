e67bcf938af4dc3f31e87362bf69c2aa51ac4dc1
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-30 06:57:33 +0530

    Add dynamic comp

---
+++ 07-special-elements/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Product from "./Product.svelte";
+  import CartItem from "./CartItem.svelte";
+
+  let renderedComp = { cmp: Product, title: "Test Product", id: "p1" };
+
+  function toggle() {
+    if (renderedComp.cmp === Product) {
+      renderedComp = { cmp: CartItem, title: "Test Cart Item", id: "p2" };
+    } else {
+      renderedComp = { cmp: Product, title: "Test Product", id: "p1" };
+    }
+  }
+</script>
+
+<button on:click={toggle}>Toggle Display</button>
+
+<svelte:component
+  this={renderedComp.cmp}
+  title={renderedComp.title}
+  id={renderedComp.id} />
