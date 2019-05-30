2618c253a46c3436856cd597f77c9052f5836cb2
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-30 07:14:36 +0530

    Add cross comp scripting

--- 07-special-elements/src/FamilyNode.svelte
+++ 07-special-elements/src/FamilyNode.svelte
@@ -1,14 +1,41 @@
+<script context="module">
+  console.log("Runs once!");
+
+  let deactivateNode;
+</script>
+
 <script>
   export let member;
+
+  let isActive;
+
+  console.log("Runs multiple times!");
+
+  function activate() {
+    if (deactivateNode) {
+      deactivateNode();
+    }
+
+    isActive = true;
+    deactivateNode = deactivate;
+  }
+
+  function deactivate() {
+    isActive = false;
+  }
 </script>
 
 <style>
   div {
     margin-left: 2rem;
   }
+
+  .active {
+    color: red;
+  }
 </style>
 
-<div>
+<div on:click={activate} class:active={isActive}>
   <h1>{member.name}</h1>
   {#if member.isParent}
     {#each member.children as child}
